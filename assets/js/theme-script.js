82dbcf3f9c1b02c9418932e049b9313e2b6fa9e4
sus hill <sus.hill.dhakal@gmail.com>
2018-04-30 21:54:50 +0545

    lll

--- assets/js/theme-script.js
+++ assets/js/theme-script.js
@@ -40,9 +40,38 @@
   });
 
   $('#top-mobile-menu').click(function(){
-  	$('.indrajeet-theme-topnavbar-collapse').toggle('slide');
+  	$('.indrajeet-theme-topnavbar-collapse').slideToggle();
   	$(this).toggleClass('open');
   });
+
+
+
+
+	$(document).ready(function() {
+			// grab the initial top offset of the navigation
+		   	var stickyNavTop = $('#header').offset().top;
+		   	
+		   	// our function that decides weather the navigation bar should have "fixed" css position or not.
+		   	var stickyNav = function(){
+			    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+			         
+			    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+			    // otherwise change it back to relative
+			    if (scrollTop > stickyNavTop) { 
+			        $('#header').addClass('sticky');
+			    } else {
+			        $('#header').removeClass('sticky'); 
+			    }
+			};
+
+			stickyNav();
+			// and run it again every time you scroll
+			$(window).scroll(function() {
+				stickyNav();
+			});
+
+
+		});
 
   $('#onload').hide();
 
