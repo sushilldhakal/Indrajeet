e04d0369c92355cf25b0e741cc109ea9a29c0f0c
sus hill <sus.hill.dhakal@gmail.com>
2018-06-12 01:17:01 +0545

    changes of version 1.0.4

--- inc/customizer/colors.php
+++ inc/customizer/colors.php
@@ -13,8 +13,8 @@ $wp_customize->add_setting('indrajeet_options[indrajeet_footer_bg_color]', array
 
 // Footer background Color Control.
 $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'indrajeet_options[indrajeet_footer_bg_color]', array(
-	'label'       => esc_html__( 'Footer Section Background Color', 'travel-log' ),
-	'description' => esc_html__( 'Footer Section Background Color', 'travel-log' ),
+	'label'       => esc_html__( 'Footer Section Background Color', 'indrajeet' ),
+	'description' => esc_html__( 'Footer Section Background Color', 'indrajeet' ),
 	'section'     => 'colors',
 	'settings'    => 'indrajeet_options[indrajeet_footer_bg_color]',
 	'priority'    => 21,
